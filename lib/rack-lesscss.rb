4ef4640da1e6e24b582bac58738518ef7c88fe73
Marcin Kulik <marcin.kulik@gmail.com>
2009-10-18 02:16:14 +0200

    added setting of content-length, cleaned up a little

--- lib/rack-lesscss.rb
+++ lib/rack-lesscss.rb
@@ -6,17 +6,19 @@ module Rack
     def initialize(app, opts)
       @app = app
       @less_path = opts[:less_path] or raise ArgumentError, "You must specify less_path (path to directory containing .less files)"
-      @css_route = opts[:css_route] || "/stylesheets"
-      @css_route = @css_route[0..-2] if @css_route[-1] == "/"
+      css_route = opts[:css_route] || "/stylesheets"
+      css_route = css_route[0..-2] if css_route[-1] == "/"
+      @css_route_regexp = /#{Regexp.escape(css_route)}\/([^\.]+)\.css/
     end
 
     def call(env)
-      if env['PATH_INFO'] =~ /#{Regexp.escape(@css_route)}\/([^\.]+)\.css/
+      if env['PATH_INFO'] =~ @css_route_regexp
         begin
           headers = { 'Content-Type' => 'text/css', 'Cache-Control' => 'private' }
           body = "/* Generated from #{$1}.less by Rack::LessCss middleware */\n\n"
           body << Less::Engine.new(get_source($1)).to_css
-          return [200, headers, body]
+          headers["Content-Length"] = body.size.to_s
+          return [200, headers, [body]]
         rescue SyntaxError, StandardError => e
           puts "Rack::LessCss Error: #{e.message}"
         end
